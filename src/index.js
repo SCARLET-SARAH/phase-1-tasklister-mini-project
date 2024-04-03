aa3da06f58b0040c2710876f96241d94f3ceb308
scarlet <sarahscarlet641@gmail.com>
2024-04-03 12:54:28 +0300

    create event.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("form#create-task-form")
+  const inputDesc = document.querySelector('form > input#new-task-description')
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    console.log(inputDesc.value)
+  })
 });
+
